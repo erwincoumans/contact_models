d190e4116f423d2f777997fbd68d8fee9e497947
pchorak <peter.c.horak@gmail.com>
2017-09-04 16:16:46 -0400

    Add smoothed contact model

---
+++ forward_convex.m
@@ -0,0 +1,54 @@
+function x_next = forward_convex(params, x, u)
+% q = [th; x; y; w; v_x; v_y]
+h = params.h; % time step (s)
+mu = params.mu; % Coloumb friction
+m = params.m; % disk mass (kg)
+r = params.r; % disk radius (m)
+m_p = params.m_p; % pusher point mass (kg)
+
+Rmax = 100;
+Rmin = 0.01;
+wmax = 0.1;
+
+% Fixed matrices
+nc = 1;
+I = eye(nc);
+U = mu*I;
+B = [0 0; 1 0; 0 1];
+M = diag([0.5*m*r^2 m_p m_p]);
+
+ctr_disk = [r*cos(x(1)); r*sin(x(1))];
+ctr_pusher = x(2:3);
+
+% Contact info
+n_hat = (ctr_pusher - ctr_disk)/norm(ctr_pusher - ctr_disk); % normal
+t_hat = [-n_hat(2); n_hat(1)]; % tangent
+psi = norm(ctr_pusher - ctr_disk) - r; % normal distance
+r_vec = ctr_disk + r*n_hat; % contact location
+% Contact Jacobians
+Gn = [r_vec(2)*n_hat(1) - r_vec(1)*n_hat(2); n_hat];
+Gt = [r_vec(2)*t_hat(1) - r_vec(1)*t_hat(2); t_hat];
+
+% Gravity
+g_vec = [-m*9.81*ctr_disk(1); 0; 0]; % pretend the pusher has perfect gravity compensation
+
+% Helper variables
+kv = x(4:6) + M\(B*u + g_vec)*h;
+G = M\[Gn Gt];
+J = [Gn Gt]';
+A = J*G; A = (A + A')/2; % J*(M\J)
+c = J*kv;
+R = (Rmin + (Rmax - Rmin)*psi/wmax)*eye(size(A));
+
+Alt = [-A(1:nc,:); -U I; -U -I];
+blt = [c(1:nc,:) + psi/h; zeros(2*nc,1)];
+
+% Solve optimization
+f = quadprog(A + R, c, Alt, blt, [], [], [zeros(nc,1); -inf(nc,1)], inf(2*nc,1), [], ...
+    optimset('Algorithm', 'interior-point-convex', 'Display', 'off'));
+
+% Calculate velocity from contact inpulses
+v_next = G*f + kv;
+x_next = [x(1:3) + h*v_next; v_next];
+
+end
