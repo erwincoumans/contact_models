42c4e2b22b4660b259bc278695dbdd2c5b4f3012
pchorak <peter.c.horak@gmail.com>
2017-09-04 13:59:40 -0400

    Disk simulation - interactive

---
+++ simulation.m
@@ -0,0 +1,53 @@
+% Parameters
+h = 0.02;
+mu = 0.3;
+m = 0.2;
+r = 0.5;
+m_p = 1;
+params = struct('h', h, 'mu', mu, 'm', m, 'r', r, 'm_p', m_p);
+
+% Disk
+angles = linspace(0, 2*pi, 20);
+xs = r*cos(angles);
+ys = r*sin(angles);
+
+% Initial pose
+x = [0; 0.5; -1; 0; 0; 0];
+ax = axes(); hold on;
+cursor = ax.CurrentPoint(1,1:2);
+disk = plot(xs + r*cos(x(1)), ys + r*sin(x(1)), 'k');
+targ = plot(cursor(1), cursor(2), '+r');
+pt = plot(x(2), x(3), 'g.');
+axis([-2 2 -2 2])
+
+% PD control
+kp = 100;
+kd = 20;
+err_last = 0;
+umax = 10;
+
+pause(1)
+
+while (true)
+    cursor = ax.CurrentPoint(1,1:2);
+    
+    % PD control
+    err = x(2:3) - cursor(:);
+    u = -kp*err - kd*(err - err_last)/h;
+    u = max(min(u, umax), -umax);
+    err_last = err;
+    
+    % Dynamics
+    x = forward_lcp(params, x, u);
+    
+    % Plotting
+    disk.XData = xs + r*cos(x(1));
+    disk.YData = ys + r*sin(x(1));
+    targ.XData = cursor(1);
+    targ.YData = cursor(2);
+    pt.XData = x(2);
+    pt.YData = x(3);
+    axis([-2 2 -2 2])
+    
+    pause(h)
+end
