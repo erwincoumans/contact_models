1554a13a73f60fed9feb7603d13237c25b18cc04
pchorak <peter.c.horak@gmail.com>
2018-01-06 09:45:09 -0500

    Improve example grasp plot

--- example_grasp.m
+++ example_grasp.m
@@ -2,6 +2,7 @@
 
 % Note: with convex contact model, the gripper opens a bit when it hits
 % the high stop. Larger h exacerbates the issue.
+clear
 
 % Parameters
 h = 0.02;
@@ -14,16 +15,29 @@ params = struct('h', h, 'mu', mu, 'm', m, 'r', r, 'm_g', m_g, 'step_fun', @forwa
 x0 = [0, r, 0, 1.2*r, -1.2*r, 0, zeros(1,6)]';
 u = [-4 4 5]';
 N = 50;
-[x, f] = gripper_sim(params, x0, repmat(u,1,N));
 
-%% Lines
+%% Simulation
 time = 0:h:h*N;
-lstyle = '-';
-plot(time, x(2,:), lstyle, 'Color', [0 0.447 0.741])
-hold on
-plot(time, x(4,:) - x(5,:), lstyle, 'Color', [0.850 0.325 0.098])
-plot(time, x(6,:), lstyle, 'Color', [0.929 0.6940 0.125])
+
+params.step_fun = @forward_lcp;
+[x{1}, ~] = gripper_sim(params, x0, repmat(u,1,N));
+params.step_fun = @forward_ccp;
+[x{2}, ~] = gripper_sim(params, x0, repmat(u,1,N));
+params.step_fun = @forward_convex;
+[x{3}, ~] = gripper_sim(params, x0, repmat(u,1,N));
+
+%% Plotting
+colors = {[0 0.447 0.741], [0.850 0.325 0.098], [0.929 0.6940 0.125]};
+styles = {'-', '--', ':'};
+
+for i = 1:3
+    plot(time, x{i}(2,:), styles{i}, 'Color', colors{1})
+    hold on
+    plot(time, x{i}(4,:) - x{i}(5,:), styles{i}, 'Color', colors{2})
+    plot(time, x{i}(6,:), styles{i}, 'Color', colors{3})
+end
 hold off
+
 legend('Disk Y-Position', 'Gripper Gap', 'Gripper Y-Position')
 xlabel('Time (sec)')
 ylabel('Position (m)')
