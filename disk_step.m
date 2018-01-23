06d2602308f94d9529b48bd162afe04732d39434
pchorak <peter.c.horak@gmail.com>
2018-01-23 18:12:39 -0500

    Add bead example

---
+++ disk_step.m
@@ -0,0 +1,35 @@
+function [y, f] = disk_step(params, x, u)
+% x = [x_pos, y_pos, theta, x_vel, y_vel, omega]
+
+% System parameters
+h = params.h;
+mu = params.mu;
+m = params.m; % disk mass
+r = params.r; % disk radius
+w = params.w; % hole radius
+step_fun = params.step_fun;
+
+M = diag([m m 0.5*m*r^2]);
+
+% Extract pose and velocity
+n = size(x,1);
+q = x(1:n/2);
+v = x(n/2+1:end);
+
+% Gravitational, external, and other forces
+Fext = [0; -9.81*m; 0] + u;
+
+% Contact normal distances (gaps)
+psi = [w - (q(2) + r)
+       w + (q(2) - r)];
+
+% Jacobian for contacts
+J = [ 0 -1  0
+      0  1  0
+     -1  0  r
+      1  0  r];
+
+[q_next, v_next, f] = step_fun(h, M, q, v, Fext, mu, psi, J);
+
+y = [q_next; v_next];
+end
--- example_insert.m
+++ example_insert.m
@@ -3,7 +3,7 @@ clear
 
 % Parameters
 h = 0.02;
-mu = 0.2*ones(4,1);
+mu = 0.2*ones(2,1);
 m = 0.2;
 r = 0.02;
 l = 0.05;
@@ -13,45 +13,45 @@ params = struct('h', h, 'mu', mu, 'm', m, 'r', r, 'l', l, ...
     'w', w, 'step_fun', @forward_lcp);
 
 x0 = [0, 0, 0, 0, 0, 0]';
-u = [0, 0, 0]';
-N = 51;
+u = [1, 0, 0]';
+N = 31;
 
 %% Simulation
 time = 0:h:h*(N-1);
 [x1, x2, x3] = deal(repmat(x0, 1, N));
 [f1, f2, f3] = deal(cell(1, N));
 
-% params.step_fun = @forward_lcp;
-% for k = 2:N
-%     [x1(:,k), f1{k}] = peg_step(params, x1(:,k-1), [2 0 0]');
-% end
-% f1 = [f1{:}];
+params.step_fun = @forward_lcp;
+for k = 2:N
+    [x1(:,k), f1{k}] = disk_step(params, x1(:,k-1), u);
+end
+f1 = [f1{:}];
 
 params.step_fun = @forward_ccp;
 for k = 2:N
-    [x2(:,k), f2{k}] = peg_step(params, x2(:,k-1), [2 0 0]');
+    [x2(:,k), f2{k}] = disk_step(params, x2(:,k-1), u);
 end
 f2 = [f2{:}];
 
 params.step_fun = @forward_convex;
 for k = 2:N
-    [x3(:,k), f3{k}] = peg_step(params, x3(:,k-1), [2 0 0]');
+    [x3(:,k), f3{k}] = disk_step(params, x3(:,k-1), u);
 end
 f3 = [f3{:}];
 
-% %% Plotting
-% plot(time, x1(2,:), '-')
-% hold on
-% plot(time, x2(2,:), '--')
-% plot(time, x3(2,:), ':')
-% hold off
-% 
-% legend('LCP','CCP','Convex')
-% xlabel('Time (sec)')
-% ylabel('Particle Y-Position (m)')
-% a = gca;
-% for k = 1:numel(a.Children)
-%     a.Children(k).LineWidth = 2;
-% end
-% a.FontSize = 14;
-% a.FontWeight = 'bold';
+%% Plotting
+plot(time, x1(4,:), '-')
+hold on
+plot(time, x2(4,:), '--')
+plot(time, x3(4,:), ':')
+hold off
+
+legend('LCP','CCP','Convex')
+xlabel('Time (sec)')
+ylabel('Disk X-Velocity (m/s)')
+a = gca;
+for k = 1:numel(a.Children)
+    a.Children(k).LineWidth = 2;
+end
+a.FontSize = 14;
+a.FontWeight = 'bold';
