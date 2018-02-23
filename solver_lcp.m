5b1fe9dcdc166d192cbcad6a26598498894a58cc
pchorak <peter.c.horak@gmail.com>
2018-02-23 13:43:19 -0500

    Remove default CFM in lcp solver

--- solver_lcp.m
+++ solver_lcp.m
@@ -26,8 +26,8 @@ b = c + [psi/h; zeros(2*nc,1)];
 
 %% Linear Complementarity Problem (LCP)
 
-% CFM
-A = A + (1e-5/h)*eye(size(A));
+% % CFM
+% A = A + (1e-5/h)*eye(size(A));
 
 % Augment with exta tangent directions and friction cone
 E = [eye(nc); eye(nc)];
